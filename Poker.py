ed8d8f01df4529f2e73fed5c0b54fb250fbfbf1e
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2022-10-13 13:06:23 +0200

    Update Poker.py

--- Poker.py
+++ Poker.py
@@ -5,15 +5,21 @@ def testSymbols(arr):
     for i in arr:
         print(i % 13)
 
-#4 verschiedene Karten % 4
+#4 verschiedene Farben % 2
 def testColors(arr):
     for i in arr:
-        print(i % 4)
+        print(i % 2)
+        
+#5 Karten ziehen
+def draw(hand):
+    for i in range(hand):
+        print(i)
 
 if __name__ == '__main__':
     pool = np.arange(52)
     #testSymbol(pool)
     #testColors(pool)
+    draw(5)
     
     
     
