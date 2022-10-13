90484756aaf672e4e4d66c68ed0502cd394a3df0
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2022-10-13 18:16:56 +0200

    update

--- Poker.py
+++ Poker.py
@@ -31,12 +31,54 @@ def drawFromPool(hand):
         
     return result
         
+#===============Kombinationen=============
+
+#Zwei Karten selbes Symbol
+def paar(hand):
+    for i in hand:
+        for j in np.delete(hand, np.where(hand == i)):
+            if i % 13 == j % 13:
+                return True
+    return False
+
+#Zwei Paare ---------------------------------
+def zweiPaare(hand): 
+    counter = 2
+    for i in hand:
+        for j in np.delete(hand, np.where(hand == i)):
+            if i % 13 == j % 13:
+                return True
+    return False
+
+#Drei Karten selbes Symbol
+def drilling(hand):
+    for i in hand:
+        counter = 2
+        for j in np.delete(hand, np.where(hand == i)):
+            if i % 13 == j % 13:
+                counter = counter - 1
+                if counter == 0:
+                    return True
+    return False
+
+#Vier Karten selbes Symbol
+def vierling(hand):
+    for i in hand:
+        counter = 3
+        for j in np.delete(hand, np.where(hand == i)):
+            if i % 13 == j % 13:
+                counter = counter - 1
+                if counter == 0:
+                    return True
+    return False
         
 
 if __name__ == '__main__':
-    #testSymbol(pool)
-    #testColors(pool)
-    print(drawFromPool(5))
+    
+    for i in range(1000):
+        hand = drawFromPool(5)
+        if vierling(hand):
+            print(hand)
     
     
     
