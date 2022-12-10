fb9e84353f2676d700ca3b234552bf2477e53614
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2022-12-10 21:43:59 +0100

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args) {
+        String a = "aa";
+        String b = "aa";
+
+
+        if (a == b){
+            System.out.println("jaa");
+        } 
+        else {
+            System.out.println("neein");
+        }
+    }
+}
