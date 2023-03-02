bb34669e3f3f9a2874b665d51257572b8050ecd7
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2023-03-02 11:36:59 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 import requests
 
-api_key = '2c8301ed15984ac1838cae030b1bb981'
-request_msg = f'https://newsapi.org/v2/top-headlines?country=at&apiKey={api_key}'
+#Ausrtria NES
+# api_key = '2c8301ed15984ac1838cae030b1bb981'
+# request_msg = f'https://newsapi.org/v2/top-headlines?country=at&apiKey={api_key}'
 
-response = requests.get(request_msg).json()
-articles = response['articles']
+# response = requests.get(request_msg).json()
+# articles = response['articles']
 
-print(articles)
+# print(articles)
 
-for a in articles:
-    print(a['title'])
-    print(a['content'])
+# for a in articles:
+#     print(a['title'])
+#     print(a['content'])
+
+stop = 'Finanzamt'
+
+allStops = requests.get("https://smartinfo.ivb.at/api/JSON/STOPS").json()
+
+for s in allStops:
+    if s['stop']['name'] == stop:
+        id = s['stop']['uid']
+
+departures = requests.get(f"https://smartinfo.ivb.at/api/JSON/PASSAGE?stopID={id}").json()
+
+for d in departures:
+    print(d)
 
